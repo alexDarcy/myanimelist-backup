4f70ba7a4a81ada5c5494c636147e33a85f26d18
Alexis Praga <praga@cerfacs.fr>
2015-01-24 20:04:24 +0100

    First version : read a XML file from the command line and output a JSON file nicely formatted.

--- main.hs
+++ main.hs
@@ -1,22 +1,54 @@
-module Main where
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 import System.Environment
 import Text.XML.HXT.Core
--- import Text.XML.HXT.Curl
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
+import Data.Monoid
+import Data.Ord
+import qualified Data.Text as T
 
+-- A very simple structure for the anime
+data Anime = Anime {
+  title :: String,
+  status :: String
+} deriving (Show, Generic)
 
-type Anime = String
+type AnimeList = [Anime]
 
--- tinstance XmlPickler Anime
---  where xpickle = xpAnime
-  
+-- Create JSON type automatically
+instance ToJSON Anime 
+
+-- Picklers, needed to read from XML
 xpAnime :: PU Anime
 xpAnime 
-  = xpElem "title" $ xpText
+  = xpElem "anime" $ 
+  xpFilterCont (hasName "series_title" 
+                <+> hasName "my_status") $ 
+  xpWrap ( uncurry Anime
+         , \s ->  (title s, status s)
+         ) $
+  xpPair ( xpElem "series_title" $ xpText )
+         ( xpElem "my_status" $ xpText)
+
+xpAnimeList :: PU AnimeList
+xpAnimeList 
+  = xpElem "myanimelist" $ 
+  xpFilterCont (hasName "anime") $ 
+  xpList $ xpAnime 
+
+-- Pretty printing for output JSON : fields are sorted by keys
+comp :: T.Text -> T.Text -> Ordering
+comp = keyOrder ["foo","bar"] `mappend` comparing T.length
+
+myConfig = Config {confIndent = 2, confCompare = comp}
 
 main :: IO ()
 main = do
   [src] <- getArgs
-  runX  (xunpickleDocument xpAnime [] src)
+  [a] <- runX  ( xunpickleDocument xpAnimeList [ withRemoveWS yes ] src)
+  B.writeFile "anime.json" $ encodePretty' myConfig a
   return ()
 
